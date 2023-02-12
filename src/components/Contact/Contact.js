2067fdd95f34be0b5ab3a9c9004ab5d96671d18a
Dawid Majchrzak <programista.dawid.majchrzak@gmail.com>
2023-02-12 19:01:31 +0100

    contact and home components added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,94 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        marginTop: 100
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 const Contact = () => {
+    const classes = useStyles();
+
     return (
-        <>
-            <h3 style={{ margin: "100px" }}>Contact</h3>
-        </>
-    )
+        <div className={classes.root}>
+            <main>
+                <div className={classes.heroContent}>
+                    <Container maxWidth="sm">
+                        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Contact Us
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Have any questions or concerns? Fill out the form below and we will get back to you as soon as possible.
+                        </Typography>
+                        <form className={classes.form} noValidate>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="name"
+                                label="Name"
+                                name="name"
+                                autoComplete="name"
+                                autoFocus
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                multiline
+                                rows={4}
+                                id="message"
+                                label="Message"
+                                name="message"
+                                autoComplete="message"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Submit
+                            </Button>
+                        </form>
+                    </Container>
+                </div>
+            </main>
+        </div>
+    );
 };
 
 export default Contact;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        marginTop: 150
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+}));
 
 const Home = () => {
+    const classes = useStyles();
+
     return (
-        <>
-            <h3 style={{ margin: "100px" }}>Home</h3>
-        </>
-    )
-}
+        <div className={classes.root}>
+            <main>
+                <div className={classes.heroContent}>
+                    <Container maxWidth="sm">
+                        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Welcome to our CBD web shop
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            We offer a variety of high-quality CBD products to meet your needs. Take a look at our products and find the perfect one for you!
+                        </Typography>
+                        <div className={classes.heroButtons}>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    {/* Add a link to your products page here */}
+                                    <a href="/products">See Our Products</a>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+            </main>
+        </div>
+    );
+};
+
 export default Home;
